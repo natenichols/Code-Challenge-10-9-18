3417193aff46cb2550434b7dee5770b7589cabe7
Nathan Nichols <n545n545@ku.edu>
2018-10-11 12:37:21 -0500

    Ex3 works, but code is not clean

--- Ex3/main.cpp
+++ Ex3/main.cpp
@@ -11,16 +11,16 @@ std::condition_variable _cond;
 bool ready = false;
 bool waiting = false;
 
+
 void doWork(std::string msg)
 {
-	std::unique_lock<std::mutex> lock(_mu);
-	
-	std::cout << msg << ": has started, is waiting" << std::endl;
 
-	//Loop Somewhere in here	
+	
+	std::unique_lock<std::mutex> lock(_mu);
 
 	//Threads block on condition_signal(2)
 	_cond.wait(lock, []{return ready;});
+
 
 	std::cout << msg << ": signaled, doing work" << std::endl;
 
@@ -28,11 +28,27 @@ void doWork(std::string msg)
 
 	std::cout << msg << ": work is done, signaling next" << std::endl;
 	
+	
 	//notify the next thread
 	_cond.notify_one();
 
-	//End Loop somewhere here
 	
+	
+}
+void loopWork(std::string msg)
+{
+	std::unique_lock<std::mutex> lock(_mu);
+	
+	//Will block on the condition variable in loop
+	std::cout << msg << ": has started, is waiting" << std::endl;
+	
+	lock.unlock();
+
+	while(true){
+		//Implemented to let each thead reach the condition variable before it is notified again
+		usleep(500);
+		doWork(msg);
+	}
 }
 int main()
 {
@@ -41,11 +57,11 @@ int main()
 
 	
 	//Start all threads (1) (usleep makes sure each string has time to get to the condition variable)
-	std::thread thread1(doWork, std::string("Thread 1"));
+	std::thread thread1(loopWork, std::string("Thread 1"));
 	usleep(1);
-	std::thread thread2(doWork, std::string("Thread 2"));
+	std::thread thread2(loopWork, std::string("Thread 2"));
 	usleep(1);
-	std::thread thread3(doWork, std::string("Thread 3"));
+	std::thread thread3(loopWork, std::string("Thread 3"));
 	usleep(1);
 	
 	
